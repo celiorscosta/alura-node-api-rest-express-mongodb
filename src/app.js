f1e357c18e513d6ff5dc79c5fae227b1bc37d49f
celiorscosta <celiorscosta@hotmail.com>
2022-09-12 09:41:49 -0300

    Primeira Entidade e Alterção do metodo get.

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
 import express from "express";
 import db from "./config/dbConfig.js";
+import livros from "./models/Livro.js"
 
 db.on("error", console.log.bind(console, "Erro de Conexão."));
-db.once("open", ()=> {console.log("Conexão com banco feita com sucesso.")});
+db.once("open", () => { console.log("Conexão com banco feita com sucesso.") });
 
 const app = express();
 
 app.use(express.json());
-
-const livros = [
-    { id: 1, "titulo": "Senhor dos Aneis" },
-    { id: 2, "titulo": "O Hobbit" }
-];
 
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node Alura');
 });
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
+    livros.find((err, livros) => {
+        res.status(200).json(livros);
+    });
 });
 
-app.get('/livros/:id', (req, res)=>{
+app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
     res.json(livros[index]);
 });
@@ -31,21 +29,21 @@ app.post('/livros', (req, res) => {
     res.status(201).send('Livro foi cadastrado com sucesso!');
 });
 
-app.put('/livros/:id', (req, res)=>{
+app.put('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.json(livros);
 });
 
 
-app.delete('/livros/:id', (req, res)=>{
-    let {id} = req.params;
+app.delete('/livros/:id', (req, res) => {
+    let { id } = req.params;
     let index = buscaLivro(id);
     livros.splice(index, 1);
     res.send(`Livro ${id} removido com sucesso!`);
 });
 
-function buscaLivro(id){
+function buscaLivro(id) {
     return livros.findIndex(livro => livro.id == id);
 }
 
