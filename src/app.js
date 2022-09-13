0409a5d15fcbeed7c605141bdf5636517784061e
celiorscosta <celiorscosta@hotmail.com>
2022-09-13 08:59:49 -0300

    refatorando app.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, "Erro de Conexão."));
 db.once("open", () => { console.log("Conexão com banco feita com sucesso.") });
+
 const app = express();
 app.use(express.json());
 routes(app);
